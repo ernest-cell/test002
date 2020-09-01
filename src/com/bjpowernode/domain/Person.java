08902b59337824676c44cfc23fcd0d37c2c5b27f
ernest <ernestjack27@gmail.com>
2020-09-01 22:48:25 +0800

    123

---
+++ src/com/bjpowernode/domain/Person.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.domain;
+
+/**
+ * ClassName:Person
+ * Package:com.bjpowernode.domain
+ * Description:
+ *
+ * @Date:2020/9/1 22:43
+ * @Author:sunzheng@njwangbo.com
+ */
+public class Person {
+    private String id;
+}
